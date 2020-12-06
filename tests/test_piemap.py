c0dada1f124deee86275dbb4423699bfb43df7fe
Stefan Hagen <stefan@hagen.link>
2020-12-06 22:04:29 +0100

    Create test_piemap.py

---
+++ tests/test_piemap.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring,unused-import,reimported
+import pytest  # type: ignore
+
+import piemap.piemap as pie
+
+
+def test_parse_ok_empty_string():
+    assert pie.parse('') is NotImplemented
