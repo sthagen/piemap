c948d81a6d9fff43a9d8ba885debb55ca1a21539
Stefan Hagen <stefan@hagen.link>
2020-12-14 18:45:41 +0100

    Added axis name circle adjust (incomplete)

--- piemap/geometry.py
+++ piemap/geometry.py
@@ -70,8 +70,62 @@ def axis_name_circle_adjust(angle, font_size_pts, text_angle, font_name, axis_na
     """
     calculate axis name placements from bounding box and angle
     """
+
+    def bounding_box_from_font(font_size_pts, text_angle, font_name, axis_name):
+        """
+        TODO(sthagen): PHP::imagettfbbox(font_size_pts, text_angle, font_name, axis_name)
+        6,7	upper left corners X,Y-Pos and 4,5	upper right corners X,Y-Pos
+        0,1	lower left corners X,Y-Pos and 2,3	lower right corners X,Y-Pos
+        
+        Returns tuple with: 
+             0,         1,         2,          3,          4,          5,          6,         7
+             lo_left_x, lo_left_y, lo_right_x, lo_right_y, hi_right_x, hi_right_y, hi_left_x, hi_left_y
+        """
+        return (100, 200, 300, None, None, None, None, 400)  # TODO(sthagen) implement real functionality
+
     axis_name_space_sep = True if axis_name_space_sep is None else axis_name_space_sep
-    return NotImplemented
+    
+    bbox = bounding_box_from_font(font_size_pts, text_angle, font_name, axis_name)
+    text_width = math.abs(bbox[2] - bbox[0])
+    text_height = math.abs(bbox[7] - bbox[1])
+
+    sep_x = 0
+    sep_y = 0
+    if axis_name_space_sep:
+        sep_x = 5
+        sep_y = 5
+
+    dx_pixel = 0
+    dy_pixel = 0
+    octant = octant_of_angle(angle)
+    if octant == 'SE':
+        dx_pixel = 0 + sep_x
+        dy_pixel = +text_height + sep_y
+    elif octant == 'NE':
+        dx_pixel = 0 + sep_x
+        dy_pixel = 0 - sep_y
+    elif octant == 'SW':
+        dx_pixel = -text_width - sep_x
+        dy_pixel = +text_height + sep_y
+    elif octant == 'NW':
+        dx_pixel = -text_width - sep_x
+        dy_pixel = 0 - sep_y
+    elif octant == 'S':
+        dx_pixel = -text_width / 2
+        dy_pixel = +text_height + 2 * sep_y
+    elif octant == 'N':
+        # exactly on top of center
+        dx_pixel = -text_width / 2
+        dy_pixel = 0 - 2 * sep_y
+    elif octant == 'W':
+        dx_pixel = -text_width - sep_x
+        dy_pixel = +text_height / 2 + 0
+    elif octant == 'E':
+        dx_pixel = +text_width + sep_x
+        dy_pixel = +text_height / 2 + 0
+
+    # Note: Unmatched octant returns (0,0)
+    return (dx_pixel, dy_pixel)
 
 
 def xy_point_from_radius_angle(radius, angle, c_x = 0, c_y = 0):
