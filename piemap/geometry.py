268f30b1c5360ffda03f1d3b831cdf6d89ed7075
Stefan Hagen <stefan@hagen.link>
2020-12-14 17:42:53 +0100

    Added left top of center per angle

--- piemap/geometry.py
+++ piemap/geometry.py
@@ -32,3 +32,10 @@ def left_bottom_of_center(angle):
     True, if angle leads to point left bottom of center
     """
     return True if angle > 90 and angle < 180 else False
+
+
+def left_top_of_center(angle):
+    """
+    True, if angle leads to point left top of center
+    """
+    return True if angle >= 180 and angle < 270 else False
