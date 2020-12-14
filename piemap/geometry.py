4607535aec1682b7cadc1298666b6817d4981272
Stefan Hagen <stefan@hagen.link>
2020-12-14 17:09:28 +0100

    Added right bottom of center and nits

--- piemap/geometry.py
+++ piemap/geometry.py
@@ -1,12 +1,20 @@
 def exact_top_of_center(angle):
     """
-    True, if $angle leads to point exactly top of center
+    True, if angle leads to point exactly top of center
     """
     return True if angle == 270 else False
 
 
 def exact_bottom_of_center(angle):
     """
-    True, if $angle leads to point exactly bottom of center
+    True, if angle leads to point exactly bottom of center
     """
     return True if angle == 90 else False
+
+
+def right_bottom_of_center(angle):
+    """
+    True, if angle leads to point right bottom of center
+    """
+    return True if angle < 90 else False
+}
