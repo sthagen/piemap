9ee39e0624bb9c4cbabe439521c9c936646ba74c
Stefan Hagen <stefan@hagen.link>
2020-12-15 12:41:00 +0100

    Added min folded from limit and max test from sample two

--- tests/test_projections.py
+++ tests/test_projections.py
@@ -23,3 +23,7 @@ def test_limit_folded_from_limit_max_sample_two_ok():
 
 def test_min_folded_from_limit_max_sample_one_ok():
     assert pr.min_folded_from_limit_max(-1, 2) == 10
+
+
+def test_min_folded_from_limit_max_sample_two_ok():
+    assert pr.min_folded_from_limit_max(5, 8) == 16
