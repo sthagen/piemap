1653ca2d6bc1cc1c6d1179c3796ba2efa5f16f71
Stefan Hagen <stefan@hagen.link>
2020-12-15 12:45:34 +0100

    Added test suite for geometry with initial test

---
+++ tests/test_geometry.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring,unused-import,reimported
+import pytest  # type: ignore
+
+import piemap.geometry as geom
+
+
+def test_exact_top_of_center_true_ok():
+    assert geom.exact_top_of_center(270) is True
