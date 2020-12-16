6f32895ccac5e62015375a279657290f9592ee16
Stefan Hagen <stefan@hagen.link>
2020-12-16 11:57:25 +0100

    Added test for adjustment and north-east

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -498,3 +498,11 @@ def test_axis_name_circle_adjust_se_ok():
     args = (angle, font_size_pts, text_angle, font_name, axis_name, axis_name_space_sep)
     assert geom.octant_of_angle(angle) == 'SE'
     assert geom.axis_name_circle_adjust(*args) == (5, 205)
+
+
+def test_axis_name_circle_adjust_ne_ok():
+    angle, font_size_pts, text_angle = 270 + 1, 1, 0
+    font_name, axis_name, axis_name_space_sep = "Invisible", "Look", None
+    args = (angle, font_size_pts, text_angle, font_name, axis_name, axis_name_space_sep)
+    assert geom.octant_of_angle(angle) == 'NE'
+    assert geom.axis_name_circle_adjust(*args) == (5, -5)
