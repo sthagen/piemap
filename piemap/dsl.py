f2085c0cb823446201b9c5b173d01b603333f844
Stefan Hagen <stefan@hagen.link>
2020-12-16 16:27:48 +0100

    What could possibly go wrong?

--- piemap/dsl.py
+++ piemap/dsl.py
@@ -1,8 +1,12 @@
+import collections
 import json
+
+import projections as pr
 
 
 NEEDED_NUMBER_OF_AXIS_MAX = 16
 NULL_STR_REP = 'NULL'
+REC_SEP = ';'
 
 
 def dumps(config):
@@ -43,12 +47,12 @@ def default_linear():
 
 def default_keys():
     """DRY."""
-    return tuple(default_linear.keys())
+    return list(default_linear().keys())
 
 
 def default_values():
     """DRY."""
-    return tuple(default_linear.values())
+    return list(default_linear().values())
 
 
 def default_folded():
@@ -63,13 +67,22 @@ def default_folded():
         'AXIS_LIMIT_FOLDED': False,
         'AXIS_MIN_FOLDED': False,
         'AXIS_VALUE': NULL_STR_REP,
-        'AXIS_UNIT' => 'dB'
+        'AXIS_UNIT': 'dB',
     }
 
 
 def unsafe(text):
     """TODO(sthagen) keep/drop list applies here."""
     return text
+
+
+def is_numeric(text):
+    """Migration artifact."""
+    try:
+        v = float(text)
+        return True
+    except TypeError:
+        return False
 
 
 def parse(text):
@@ -94,96 +107,87 @@ def parse(text):
         axis_values_rows_req_string = unsafe(text)
         axis_values_rows_req = axis_values_rows_req_string.split()
         n_axis_rows_req = len(axis_values_rows_req)
-        foreach(array_slice($axisValuesRowsReq,0,$neededNumberOfAxisMax) as $n => $rowString) {
-            $axisValues = $axisDefaultValues;
-            $axisValuesCand = explode(';',$rowString);
-            if (count($axisValues) == count($axisValuesCand)) {
-                foreach($axisValuesCand as $i => $v) {
-                    if ($v != '') {
-                        $axisValues[$i] = $v;
-                    }
-                }
-            }
-            $axisMap = array_combine($axisDefaultKeys,$axisValues);
-            $numericAxisTypes = array('LINEAR','FOLDED');
-            if($axisMap['AXIS_INDEX'] == '') {
-                $axisMap['AXIS_INDEX'] = $n;
-            }
-            else {
-                $indexCand = strval($axisMap['AXIS_INDEX']);
-                $iCFC = strval(intval($indexCand));
-                $axisMap['AXIS_INDEX'] = intval($iCFC);
-                if ($indexCand !== $iCFC) {
-                    $infoQueue[] = 'NOK \''.$indexCand.'\' index requested, accepted as \''.$iCFC.'\'';
-                }
-                else {
-                    $infoQueue[] = ' OK \''.$indexCand.'\' index requested, accepted as \''.$iCFC.'\'';
-                }
-            }
-            if (in_array($axisMap['AXIS_TYPE'], $numericAxisTypes)) {
-                $axisMap['AXIS_MIN'] = StarPlot_minFromLimitMax($axisMap['AXIS_LIMIT'], $axisMap['AXIS_MAX']);
-                if($axisMap['AXIS_VALUE'] != $nullStrRepr and !is_numeric($axisMap['AXIS_VALUE'])) {
-                    $axisMap['AXIS_VALUE'] = $nullStrRepr;
-                }
-            }
-            if ($axisMap['AXIS_TYPE'] == 'FOLDED') {
-                $axisMap['AXIS_LIMIT_FOLDED'] = StarPlot_limitFoldedFromLimitMax($axisMap['AXIS_LIMIT'], $axisMap['AXIS_MAX']);
-                $axisMap['AXIS_MIN_FOLDED'] = StarPlot_minFoldedFromLimitMax($axisMap['AXIS_LIMIT'], $axisMap['AXIS_MAX']);
-            }
-            $axisValues = array_values($axisMap);
-            $someAxisMaps[] = $axisMap;
-        }
-        $nAxisRows = count($someAxisMaps);
-        if($nAxisRowsReq > $nAxisRows) {
-            $infoQueue[] = $nAxisRowsReq.' dimensions requested, but only '.$nAxisRows.' accepted. Maximum is '.$neededNumberOfAxisMax;
-        }
-        $bestEffortReOrderMap = array();
-        $collectIndexCandList = array();
-        foreach($someAxisMaps as $x => $data) {
-            $indexCand = $data['AXIS_INDEX'];
-            $iCFC = strval(intval($indexCand));
-            $collectIndexCandList[] = $iCFC;
-            if(!is_numeric($indexCand) or $iCFC != $indexCand or $indexCand < 0 or $indexCand >= $nAxisRows) {
-                $hasIndexCollision = True;
-                $conflictReason = 'NO_INTEGER';
-                if ($iCFC != $indexCand) {
-                    $conflictReason = 'DC_INTEGER';
-                }
-                if ($indexCand < 0) {
-                    $conflictReason = 'LT_ZERO';
-                }
-                elseif ($indexCand >= $nAxisRows) {
-                    $conflictReason = 'GT_NROW';
-                }
-                $infoQueue[] = 'Conflicting index rules. Failing IndexCand is '.$indexCand.', reason is '.$conflictReason;
-            }
-            if($indexCand != $x) {
-                $hasIndexOrderMismatch = True;
-                $infoQueue[] = 'Index positions not ordered. Misplaced IndexCand is '.$indexCand.', found at '.$x;
-            }
-            $bestEffortReOrderMap[$indexCand] = $data;
-        }
-        $collectIndexCandSet = array_unique($collectIndexCandList);
-        if(count($collectIndexCandSet) != count($collectIndexCandList)) {
-            $hasIndexCollision = True;
-            $histo = array_count_values($collectIndexCandList);
-            $blameList = array();
-            foreach($histo as $xx => $nn) {
-                if($nn != 1) {
-                    $blameList[] = $xx;
-                }
-            }
-            sort($blameList);
-            $infoQueue[] = 'Conflicting index positions. Failing IndexCand/s is/are ['.implode(', ',$blameList).'], reason is '.'NONUNIQUE_INDEX';    
-        }
-        if(!$hasIndexCollision and $hasIndexOrderMismatch) {
-            ksort($bestEffortReOrderMap);
-            $someAxisMaps = array();
-            foreach($bestEffortReOrderMap as $k => $data) {
-                $someAxisMaps[] = $data;
-            }
-        }
-    }
+        for n, row_string in enumerate(axis_values_rows_req[:NEEDED_NUMBER_OF_AXIS_MAX]):  # Was: array_slice(axis_values_rows_req, 0, NEEDED_NUMBER_OF_AXIS_MAX)
+            axis_values = default_values()
+            axis_values_cand = row_string.split(REC_SEP)
+            if len(axis_values) == len(axis_values_cand):
+                for i, v in axis_values_cand:
+                    if v != '':
+                        axis_values[i] = v
+
+            axis_map = dict(zip(default_keys(), axis_values))
+            numeric_axis_types = ('LINEAR', 'FOLDED')
+            if axis_map['AXIS_INDEX'] == '':
+                axis_map['AXIS_INDEX'] = n
+            else:
+                index_cand = str(axis_map['AXIS_INDEX'])
+                i_cfc = str(int(index_cand))
+                axis_map['AXIS_INDEX'] = int(i_cfc)
+                if index_cand != i_cfc:  # Was !== in PHP
+                    info_queue.append(f"NOK '{index_cand}' index requested, accepted as '{i_cfc}'")
+                else:
+                    info_queue.append(f" OK {index_cand}' index requested, accepted as '{i_cfc}'")
+
+            if axis_map['AXIS_TYPE'] in numeric_axis_types:
+                axis_map['AXIS_MIN'] = pr.min_from_limit_max(axis_map['AXIS_LIMIT'], axis_map['AXIS_MAX'])
+                if axis_map['AXIS_VALUE'] != NULL_STR_REP and not is_numeric(axis_map['AXIS_VALUE']):
+                    axis_map['AXIS_VALUE'] = NULL_STR_REP
+
+            if axis_map['AXIS_TYPE'] == 'FOLDED':
+                axis_map['AXIS_LIMIT_FOLDED'] = pr.limit_folded_from_limit_max(axis_map['AXIS_LIMIT'], axis_map['AXIS_MAX'])
+                axis_map['AXIS_MIN_FOLDED'] = pr.min_folded_from_limit_max(axis_map['AXIS_LIMIT'], axis_map['AXIS_MAX'])
+
+            axis_values = list(axis_map.keys())
+            some_axis_maps.append(axis_map)
+
+        n_axis_rows = len(some_axis_maps)
+        if n_axis_rows_req > n_axis_rows:
+            info_queue.append(f'{n_axis_rows_req} dimensions requested, but only {n_axis_rows} accepted. Maximum is {NEEDED_NUMBER_OF_AXIS_MAX}')
+
+        best_effort_re_order_map = {}
+        collect_index_cand_list = []
+        for x, data in some_axis_maps:
+            index_cand = data['AXIS_INDEX']
+            iCFC = str(int(index_cand))
+            collect_index_cand_list.append(iCFC)
+            if not is_numeric(index_cand) or iCFC != index_cand or index_cand < 0 or index_cand >= n_axis_rows:
+                has_index_collision = True
+                conflict_reason = 'NO_INTEGER'
+                if iCFC != index_cand:
+                    conflict_reason = 'DC_INTEGER'
+
+                if index_cand < 0:
+                    conflict_reason = 'LT_ZERO'
+
+                elif index_cand >= n_axis_rows:
+                    conflict_reason = 'GT_NROW'
+
+                info_queue.append(f'Conflicting index rules. Failing IndexCand is {index_cand}, reason is {conflict_reason}')
+
+            if index_cand != x:
+                has_index_order_mismatch = True
+                info_queue.append(f'Index positions not ordered. Misplaced IndexCand is {index_cand}, found at {x}')
+
+            best_effort_re_order_map[index_cand] = data
+
+        collect_index_cand_set = set(collect_index_cand_list)
+        if len(collect_index_cand_set) != len(collect_index_cand_list):
+            has_index_collision = True
+            hist = collections.Counter(collect_index_cand_list)
+            blame_list = []
+            for xx, nn in hist.items():
+                if nn != 1:
+                    blame_list.append(xx)
+
+            blame_list.sort()
+            info_queue.append(f'Conflicting index positions. Failing IndexCand/s is/are [{", ".join(blame_list)}], reason is NONUNIQUE_INDEX')
+
+        if not has_index_collision and has_index_order_mismatch:
+            some_axis_maps = []
+            for k, data in sorted(best_effort_re_order_map.items()):
+                some_axis_maps.append(data)
+
+    """
     $normalizedInputDataRows = array();
     foreach($someAxisMaps as $x => $data) {
         $axisValues = array_values($data);
@@ -222,5 +226,5 @@ def parse(text):
     }
     echo '</body>'."\n";
     echo '</html>'."\n";
-    return True;
-}
+    """
+    return True
