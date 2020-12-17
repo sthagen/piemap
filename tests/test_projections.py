9313972e7416f7328e0d73b7f9f6d639f5fe78ac
Stefan Hagen <stefan@hagen.link>
2020-12-17 16:39:38 +0100

    Added i-i test for min folded calc

--- tests/test_projections.py
+++ tests/test_projections.py
@@ -93,3 +93,9 @@ def test_limit_folded_from_limit_max_int_float(limit, maximum):
 def test_limit_folded_from_limit_max_float_float(limit, maximum):
     limit_folded = pr.limit_folded_from_limit_max(limit, maximum)
     assert isinstance(limit_folded, (float, int))
+
+
+@given(limit=st.integers(), maximum=st.integers())
+def test_min_folded_from_limit_max_int_int(limit, maximum):
+    minimum_folded = pr.min_folded_from_limit_max(limit, maximum)
+    assert isinstance(minimum_folded, (float, int))
