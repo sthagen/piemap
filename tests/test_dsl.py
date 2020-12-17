b2d4b5f50b7ec31c0acbd57f79479dec4cead71f
Stefan Hagen <stefan@hagen.link>
2020-12-17 14:38:16 +0100

    Added and refactored test data

--- tests/test_dsl.py
+++ tests/test_dsl.py
@@ -4,14 +4,37 @@ import pytest  # type: ignore
 
 import piemap.dsl as dsl
 
+D1F_TEXT = """\
+0;D1FX;FOLDED;;12;15;;;6;dB
+"""
+
+D1F_AST = [
+    {
+        'AXIS_INDEX': 0,
+        'AXIS_LIMIT': 12,
+        'AXIS_LIMIT_FOLDED': 18,
+        'AXIS_MAX': 15,
+        'AXIS_META': '',
+        'AXIS_MIN': 9,
+        'AXIS_MIN_FOLDED': 21,
+        'AXIS_NAME': 'D1FX',
+        'AXIS_TYPE': 'FOLDED',
+        'AXIS_UNIT': 'dB',
+        'AXIS_VALUE': 6,
+    },
+]
+
+D1F_DIAG = [' OK index (0) requested, accepted as (0)']
+
 DEFAULT_TEXT_0 = ''
 
 DEFAULT_GENERATED_0 = [
     {'AXIS_INDEX': 0,
      'AXIS_LIMIT': 0.8,
      'AXIS_LIMIT_FOLDED': False,
-     'AXIS_MAX': 1.0,
-     'AXIS_MIN': 0.0,
+     'AXIS_MAX': 1,
+     'AXIS_META': '',
+     'AXIS_MIN': 0,
      'AXIS_MIN_FOLDED': False,
      'AXIS_NAME': 'Dimension',
      'AXIS_TYPE': 'LINEAR',
@@ -22,8 +45,9 @@ DEFAULT_GENERATED_0 = [
         'AXIS_INDEX': 1,
         'AXIS_LIMIT': 0.8,
         'AXIS_LIMIT_FOLDED': False,
-        'AXIS_MAX': 1.0,
-        'AXIS_MIN': 0.0,
+        'AXIS_MAX': 1,
+        'AXIS_META': '',
+        'AXIS_MIN': 0,
         'AXIS_MIN_FOLDED': False,
         'AXIS_NAME': 'Dimension2',
         'AXIS_TYPE': 'LINEAR',
@@ -34,8 +58,9 @@ DEFAULT_GENERATED_0 = [
         'AXIS_INDEX': 2,
         'AXIS_LIMIT': 0.8,
         'AXIS_LIMIT_FOLDED': False,
-        'AXIS_MAX': 1.0,
-        'AXIS_MIN': 0.0,
+        'AXIS_MAX': 1,
+        'AXIS_META': '',
+        'AXIS_MIN': 0,
         'AXIS_MIN_FOLDED': False,
         'AXIS_NAME': 'DimensionFolded',
         'AXIS_TYPE': 'FOLDED',
@@ -53,68 +78,112 @@ DEFAULT_TEXT_1 = """\
 DEFAULT_PARSED_1 = [
     {
         'AXIS_INDEX': 0,
-        'AXIS_LIMIT': 0.8,
-        'AXIS_LIMIT_FOLDED': False,
-        'AXIS_MAX': 1.0,
-        'AXIS_MIN': 0.4666666666666668,
-        'AXIS_MIN_FOLDED': False,
-        'AXIS_NAME': 'Dimension',
-        'AXIS_TYPE': 'LINEAR',
-        'AXIS_UNIT': '1',
+        'AXIS_LIMIT': 12,
+        'AXIS_LIMIT_FOLDED': 18,
+        'AXIS_MAX': 15,
+        'AXIS_META': '',
+        'AXIS_MIN': 7,
+        'AXIS_MIN_FOLDED': 23,
+        'AXIS_NAME': 'D1F',
+        'AXIS_TYPE': 'FOLDED',
+        'AXIS_UNIT': 'dB',
         'AXIS_VALUE': 'NULL',
     },
 ]
 
-DEFAULT_DIAGNOSTICS_1 = []
+DEFAULT_DIAGNOSTICS_1 = [' OK index (0) requested, accepted as (0)']
 
 DEFAULT_TEXT_2 = """\
-;L 0;;;1;2;NULL;
-;L 1;;;1;2;-0.1;
+;L 0;;;1;2;;;NULL;
+;L 1;;;1;2;;;-0.1;
 """
 
 DEFAULT_PARSED_2 = [
-    {'AXIS_INDEX': 0,
-     'AXIS_LIMIT': 0.8,
-     'AXIS_LIMIT_FOLDED': False,
-     'AXIS_MAX': 1.0,
-     'AXIS_MIN': 0.4666666666666668,
-     'AXIS_MIN_FOLDED': False,
-     'AXIS_NAME': 'Dimension',
-     'AXIS_TYPE': 'LINEAR',
-     'AXIS_UNIT': '1',
-     'AXIS_VALUE': 'NULL',
-     },
     {
-        'AXIS_INDEX': 1,
-        'AXIS_LIMIT': 0.8,
+        'AXIS_INDEX': 0,
+        'AXIS_LIMIT': 1,
         'AXIS_LIMIT_FOLDED': False,
-        'AXIS_MAX': 1.0,
-        'AXIS_MIN': 0.4666666666666668,
+        'AXIS_MAX': 2,
+        'AXIS_META': '',
+        'AXIS_MIN': -0.6666666666666666,
         'AXIS_MIN_FOLDED': False,
-        'AXIS_NAME': 'Dimension',
+        'AXIS_NAME': 'L 0',
         'AXIS_TYPE': 'LINEAR',
         'AXIS_UNIT': '1',
         'AXIS_VALUE': 'NULL',
+    },
+    {
+        'AXIS_INDEX': 1,
+        'AXIS_LIMIT': 1,
+        'AXIS_LIMIT_FOLDED': False,
+        'AXIS_MAX': 2,
+        'AXIS_META': '',
+        'AXIS_MIN': -0.6666666666666666,
+        'AXIS_MIN_FOLDED': False,
+        'AXIS_NAME': 'L 1',
+        'AXIS_TYPE': 'LINEAR',
+        'AXIS_UNIT': '1',
+        'AXIS_VALUE': -0.1,
     },
 ]
 
 DEFAULT_DIAGNOSTICS_2 = []
 
-DEFAULT_TEXT_3 = """\
-;F 1;FOLDED;;1;2;2;
-;F 2;FOLDED;;1;2;0;
-;F 3;FOLDED;;1;2;0.25;
+DEFAULT_TEXT_2_UNORDERED = """\
+1;L 0;;;1;2;;;NULL;
+0;L 1;;;1;2;;;-0.1;
 """
 
-DEFAULT_PARSED_3 = [
+DEFAULT_PARSED_2_UNORDERED = [
     {
         'AXIS_INDEX': 0,
-        'AXIS_LIMIT': 0.8,
+        'AXIS_LIMIT': 1,
         'AXIS_LIMIT_FOLDED': False,
-        'AXIS_MAX': 1.0,
-        'AXIS_MIN': 0.4666666666666668,
+        'AXIS_MAX': 2,
+        'AXIS_META': '',
+        'AXIS_MIN': -0.6666666666666666,
         'AXIS_MIN_FOLDED': False,
-        'AXIS_NAME': 'Dimension',
+        'AXIS_NAME': 'L 1',
+        'AXIS_TYPE': 'LINEAR',
+        'AXIS_UNIT': '1',
+        'AXIS_VALUE': -0.1,
+    },
+    {'AXIS_INDEX': 1,
+     'AXIS_LIMIT': 1,
+     'AXIS_LIMIT_FOLDED': False,
+     'AXIS_MAX': 2,
+     'AXIS_META': '',
+     'AXIS_MIN': -0.6666666666666666,
+     'AXIS_MIN_FOLDED': False,
+     'AXIS_NAME': 'L 0',
+     'AXIS_TYPE': 'LINEAR',
+     'AXIS_UNIT': '1',
+     'AXIS_VALUE': 'NULL',
+     },
+]
+
+DEFAULT_DIAGNOSTICS_2_UNORDERED = [
+    ' OK index (1) requested, accepted as (1)',
+    ' OK index (0) requested, accepted as (0)',
+    'Index positions not ordered. Misplaced IndexCand is 1, found at 0',
+    'Index positions not ordered. Misplaced IndexCand is 0, found at 1',
+]
+
+DEFAULT_TEXT_2_COLLISION = """\
+333;L 0;;;1;2;;;NULL;
+333;L 1;;;1;2;;;-0.1;;
+"""
+
+DEFAULT_PARSED_2_COLLISION = [
+    {
+        'AXIS_INDEX': 333,
+        'AXIS_LIMIT': 1,
+        'AXIS_LIMIT_FOLDED': False,
+        'AXIS_MAX': 2,
+        'AXIS_META': '',
+        'AXIS_MIN': -0.6666666666666666,
+        'AXIS_MIN_FOLDED': False,
+        'AXIS_NAME': 'L 0',
         'AXIS_TYPE': 'LINEAR',
         'AXIS_UNIT': '1',
         'AXIS_VALUE': 'NULL',
@@ -124,18 +193,7 @@ DEFAULT_PARSED_3 = [
         'AXIS_LIMIT': 0.8,
         'AXIS_LIMIT_FOLDED': False,
         'AXIS_MAX': 1.0,
-        'AXIS_MIN': 0.4666666666666668,
-        'AXIS_MIN_FOLDED': False,
-        'AXIS_NAME': 'Dimension',
-        'AXIS_TYPE': 'LINEAR',
-        'AXIS_UNIT': '1',
-        'AXIS_VALUE': 'NULL',
-    },
-    {
-        'AXIS_INDEX': 2,
-        'AXIS_LIMIT': 0.8,
-        'AXIS_LIMIT_FOLDED': False,
-        'AXIS_MAX': 1.0,
+        'AXIS_META': '',
         'AXIS_MIN': 0.4666666666666668,
         'AXIS_MIN_FOLDED': False,
         'AXIS_NAME': 'Dimension',
@@ -145,7 +203,80 @@ DEFAULT_PARSED_3 = [
     },
 ]
 
+DEFAULT_DIAGNOSTICS_2_COLLISION = [
+    ' OK index (333) requested, accepted as (333)',
+    'Conflicting index rules. Failing IndexCand is 333, reason is GT_NROW',
+    'Index positions not ordered. Misplaced IndexCand is 333, found at 0',
+]
+
+DEFAULT_TEXT_3 = """\
+;F 1;FOLDED;;1;2;;;2;
+;F 2;FOLDED;;1;2;;;0;
+;F 3;FOLDED;;1;2;;;0.25;
+"""
+
+DEFAULT_PARSED_3 = [
+    {
+        'AXIS_INDEX': 0,
+        'AXIS_LIMIT': 1,
+        'AXIS_LIMIT_FOLDED': 3,
+        'AXIS_MAX': 2,
+        'AXIS_META': '',
+        'AXIS_MIN': -0.6666666666666666,
+        'AXIS_MIN_FOLDED': 4.666666666666667,
+        'AXIS_NAME': 'F 1',
+        'AXIS_TYPE': 'FOLDED',
+        'AXIS_UNIT': 'dB',
+        'AXIS_VALUE': 2,
+    },
+    {'AXIS_INDEX': 1,
+     'AXIS_LIMIT': 1,
+     'AXIS_LIMIT_FOLDED': 3,
+     'AXIS_MAX': 2,
+     'AXIS_META': '',
+     'AXIS_MIN': -0.6666666666666666,
+     'AXIS_MIN_FOLDED': 4.666666666666667,
+     'AXIS_NAME': 'F 2',
+     'AXIS_TYPE': 'FOLDED',
+     'AXIS_UNIT': 'dB',
+     'AXIS_VALUE': 0,
+     },
+    {'AXIS_INDEX': 2,
+     'AXIS_LIMIT': 1,
+     'AXIS_LIMIT_FOLDED': 3,
+     'AXIS_MAX': 2,
+     'AXIS_META': '',
+     'AXIS_MIN': -0.6666666666666666,
+     'AXIS_MIN_FOLDED': 4.666666666666667,
+     'AXIS_NAME': 'F 3',
+     'AXIS_TYPE': 'FOLDED',
+     'AXIS_UNIT': 'dB',
+     'AXIS_VALUE': 0.25,
+     }, ]
+
 DEFAULT_DIAGNOSTICS_3 = []
+
+D7B_TEXT = """\
+;D7B;BIMONOTONE;0;80;100;;;40;%;SHOW_MIN
+"""
+
+D7B_AST = [
+    {
+        'AXIS_INDEX': 0,
+        'AXIS_LIMIT': 80,
+        'AXIS_LIMIT_FOLDED': False,
+        'AXIS_MAX': 100,
+        'AXIS_META': 'SHOW_MIN',
+        'AXIS_MIN': 0,
+        'AXIS_MIN_FOLDED': False,
+        'AXIS_NAME': 'D7B',
+        'AXIS_TYPE': 'BIMONOTONE',
+        'AXIS_UNIT': '%',
+        'AXIS_VALUE': 40,
+    },
+]
+
+D7B_DIAG = []
 
 
 def test_dumps_stub_ok():
