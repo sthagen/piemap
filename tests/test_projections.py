d3c1468e360da104a1dd479ae2390076acab2d61
Stefan Hagen <stefan@hagen.link>
2020-12-24 17:48:34 +0100

    Update test_projections.py

--- tests/test_projections.py
+++ tests/test_projections.py
@@ -119,6 +119,10 @@ def test_domain_ordered_from_domain_sample_two_ok():
     assert pr.domain_ordered_from_domain(ORDERED_SAMPLE_TWO) == ORDERED_SAMPLE_TWO
 
 
+def test_domain_ordered_from_domain_empty_ok():
+    assert pr.domain_ordered_from_domain([]) == 'NULL'
+
+
 @given(limit=st.integers(), maximum=st.integers())
 def test_min_from_limit_max_int_int(limit, maximum):
     minimum = pr.min_from_limit_max(limit, maximum)
