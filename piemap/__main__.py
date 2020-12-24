bc21ffe9f0294ba0d133627d8fc8331dba9f530e
Stefan Hagen <stefan@hagen.link>
2020-12-24 17:57:02 +0100

    Fixed import in untested main module and moved pragma into correct place

--- piemap/__main__.py
+++ piemap/__main__.py
@@ -2,7 +2,7 @@
 # pylint: disable=missing-docstring
 import sys
 
-from pie_map.cli import main
+from piemap.cli import main
 
-if __name__ == "__main__":  # pragma: no cover
-    sys.exit(main(sys.argv[1:]))
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))  # pragma: no cover
