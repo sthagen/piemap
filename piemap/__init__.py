ac16ac40ddf06fd054e42e814fd71c1856de670e
Stefan Hagen <stefan@hagen.link>
2022-07-31 18:42:51 +0200

    Bumped the impl version for the initial release

--- piemap/__init__.py
+++ piemap/__init__.py
@@ -15,8 +15,8 @@ DEFAULT_CONFIG_NAME = '.piemap.json'
 DEFAULT_LF_ONLY = 'YES'
 
 # [[[fill git_describe()]]]
-__version__ = '2022.7.28+parent.9fb117a4'
-# [[[end]]] (checksum: a143e8c8551090fa81324b0683ba1a40)
+__version__ = '2022.7.30+parent.d88d6233'
+# [[[end]]] (checksum: 676141ef3144a8ea6ce2493608afe68f)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
