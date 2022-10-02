58062edecf363be9b1ad8f6a4f0e2f4898bc8fb9
Stefan Hagen <stefan@hagen.link>
2022-10-02 11:06:20 +0200

    Added the usual logging boilerplate to the package

--- piemap/__init__.py
+++ piemap/__init__.py
@@ -1,5 +1,9 @@
 """One-view visualization of grouped characterizations (Quality Pie)."""
+import datetime as dti
+import logging
 import os
+import pathlib
+from typing import no_type_check
 
 # [[[fill git_describe()]]]
 __version__ = '2022.7.30+parent.d88d6233'
@@ -20,3 +24,37 @@ ENCODING = 'utf-8'
 ENCODING_ERRORS_POLICY = 'ignore'
 DEFAULT_CONFIG_NAME = '.piemap.json'
 DEFAULT_LF_ONLY = 'YES'
+log = logging.getLogger()  # Module level logger is sufficient
+LOG_FOLDER = pathlib.Path('logs')
+LOG_FILE = f'{APP_ALIAS}.log'
+LOG_PATH = pathlib.Path(LOG_FOLDER, LOG_FILE) if LOG_FOLDER.is_dir() else pathlib.Path(LOG_FILE)
+LOG_LEVEL = logging.INFO
+
+TS_FORMAT_LOG = '%Y-%m-%dT%H:%M:%S'
+TS_FORMAT_PAYLOADS = '%Y-%m-%d %H:%M:%S.%f UTC'
+
+
+@no_type_check
+def formatTime_RFC3339(self, record, datefmt=None):  # noqa
+    """HACK A DID ACK we could inject .astimezone() to localize ..."""
+    return dti.datetime.fromtimestamp(record.created, dti.timezone.utc).isoformat()  # pragma: no cover
+
+
+@no_type_check
+def init_logger(name=None, level=None):
+    """Initialize module level logger"""
+    global log  # pylint: disable=global-statement
+
+    log_format = {
+        'format': '%(asctime)s %(levelname)s [%(name)s]: %(message)s',
+        'datefmt': TS_FORMAT_LOG,
+        # 'filename': LOG_PATH,
+        'level': LOG_LEVEL if level is None else level,
+    }
+    logging.Formatter.formatTime = formatTime_RFC3339
+    logging.basicConfig(**log_format)
+    log = logging.getLogger(APP_ENV if name is None else name)
+    log.propagate = True
+
+
+init_logger(name=APP_ENV, level=logging.DEBUG if DEBUG else None)
