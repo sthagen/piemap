99d11f4984a39af29a8b75192770d202972aaf94
Stefan Hagen <stefan@hagen.link>
2022-10-02 11:04:17 +0200

    Reordered source to harmonize and removed the typing specialties - we may have to bump the minimal python version

--- piemap/__init__.py
+++ piemap/__init__.py
@@ -1,6 +1,13 @@
 """One-view visualization of grouped characterizations (Quality Pie)."""
 import os
-from typing import List
+
+# [[[fill git_describe()]]]
+__version__ = '2022.7.30+parent.d88d6233'
+# [[[end]]] (checksum: 676141ef3144a8ea6ce2493608afe68f)
+__version_info__ = tuple(
+    e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
+)
+__all__: list[str] = []
 
 APP_NAME = 'One-view visualization of grouped characterizations (Quality Pie).'
 APP_ALIAS = 'piemap'
@@ -13,11 +20,3 @@ ENCODING = 'utf-8'
 ENCODING_ERRORS_POLICY = 'ignore'
 DEFAULT_CONFIG_NAME = '.piemap.json'
 DEFAULT_LF_ONLY = 'YES'
-
-# [[[fill git_describe()]]]
-__version__ = '2022.7.30+parent.d88d6233'
-# [[[end]]] (checksum: 676141ef3144a8ea6ce2493608afe68f)
-__version_info__ = tuple(
-    e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
-)
-__all__: List[str] = []
