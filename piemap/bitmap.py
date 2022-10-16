b167c56dfd246afa7788c08dd25a5f9bb93c2617
Stefan Hagen <stefan@hagen.link>
2022-10-16 18:41:52 +0200

    Fixed diameter vs radius confusion, shrank the central noise silencing disk, added axis end labels

--- piemap/bitmap.py
+++ piemap/bitmap.py
@@ -2,13 +2,16 @@ from typing import no_type_check
 
 from PIL import Image, ImageDraw, ImageFont  # type: ignore
 
+import piemap.geometry as geom
 from piemap import (
     ANGLE_MAX,
     ANGLE_OFF,
+    CIRCLE_CENTER,
     GRAY,
     GREEN,
     HEIGHT,
     HEIGHT_OFF,
+    LABEL_SIZE,
     LINE_WIDTH,
     PIE_BOX,
     RADIUS,
@@ -54,7 +57,7 @@ def end_of(n: int, n_max: int) -> float:
 def bbox_from_radii(ref_r: int, r: float) -> BBox:
     """Return bbox as left upper and right lower x,y tuples from radius within reference radius"""
     shift = ref_r - r
-    return (TOP_LEFT_X + shift, TOP_LEFT_Y + shift), (TOP_LEFT_X + r, TOP_LEFT_Y + r)
+    return (TOP_LEFT_X + shift, TOP_LEFT_Y + shift), (TOP_LEFT_X + ref_r + r, TOP_LEFT_Y + ref_r + r)
 
 
 @no_type_check
@@ -89,6 +92,14 @@ def draw_titles(engine, title: str, subtitle: str) -> None:
 
 
 @no_type_check
+def draw_label_at(engine, label: str, at: Coord) -> None:
+    """Draw the label starting at position."""
+    fnt = ImageFont.truetype(FONT_PATH, LABEL_SIZE)
+    l_len = engine.textlength(label, font=fnt)
+    engine.multiline_text((at[0] - int(l_len / 2), at[1]), label, font=fnt, fill=(0, 0, 0), align='center')
+
+
+@no_type_check
 def render(values: tuple[float | None, ...]) -> None:
     """Make importable to support tests."""
     n_dim = len(values)
@@ -103,7 +114,7 @@ def render(values: tuple[float | None, ...]) -> None:
     draw.pieslice(bbox_from_radii(RADIUS, RADIUS * 0.80), *fake_full_circle(ANGLE_OFF), fill=RED)
 
     # Inner disc to hide singularity noise at center
-    draw.pieslice(bbox_from_radii(RADIUS, RADIUS * 0.10), *fake_full_circle(ANGLE_OFF), fill=GRAY)
+    draw.pieslice(bbox_from_radii(RADIUS, RADIUS * 0.03), *fake_full_circle(ANGLE_OFF), fill=GRAY)
 
     # The sectors
     for n, val in enumerate(values):
@@ -114,11 +125,22 @@ def render(values: tuple[float | None, ...]) -> None:
         color = YELLOW if val < RADIUS * 0.80 else GREEN
         draw.pieslice(bbox_from_radii(RADIUS, val), start_of(n, n_dim), end_of(n, n_dim), fill=color)
 
-    # The axes
-    for n in range(n_dim):
+    draw_label_at(draw, '^', CIRCLE_CENTER)  # protonly
+    draw_label_at(draw, 'L', (CIRCLE_CENTER[0] - RADIUS, CIRCLE_CENTER[1] + RADIUS))  # protonly
+    # The axes and dim=value labels
+    for n, val in enumerate(values):
         draw.pieslice(
             extrude_bbox_by(PIE_BOX, 15), *fake_line(middle_of(n, n_dim)), fill=GRAY, outline=GRAY, width=LINE_WIDTH
         )
+        coords = geom.xy_point_from_radius_angle(RADIUS, middle_of(n, n_dim), *CIRCLE_CENTER)
+        if val is None:
+            val_disp = 'n/a'
+        else:
+            val_disp = f'{round(val, 1)}|{round(val/RADIUS, 1)}'  # |{[round(c, 1) for c in coords]}'
+            # ang_quest = (middle_of(n, n_dim) - ANGLE_OFF) % 360
+            # val_disp = f'{round(ang_quest, 1)}|{[round(c, 1) for c in coords]}'
+        # draw_label_at(draw, f'd[{n}]={val_disp}', (PIE_BOX[0][0], PIE_BOX[0][1] + n * LABEL_SIZE))
+        draw_label_at(draw, f'd[{n}]={val_disp}', coords)
 
     # outer circle (axis marker joining all dimensions)
     draw.pieslice(
